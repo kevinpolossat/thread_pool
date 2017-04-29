1af5ad79627b742f2b24aecc3ff880a250b42d2b
kevin <kevin.polossat@epitech.eu>
2017-04-29 02:30:59 +0200

    ThreadPool: add submit and push methode

---
+++ src/ThreadPool.hh
@@ -0,0 +1,74 @@
+//
+// Created by kevin on 28/04/17.
+//
+
+#ifndef THREAD_POOL_THREADPOOL_HH
+#define THREAD_POOL_THREADPOOL_HH
+
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+#include <deque>
+#include <thread>
+#include <algorithm>
+
+class ThreadPool {
+public:
+    ThreadPool(std::size_t nThread = std::thread::hardware_concurrency()): _run(true) {
+        if (!nThread) {
+            throw std::range_error("nThread must be strictly positive");
+        }
+        for (std::size_t i = 0; i < nThread; ++i) {
+            _workers.emplace_back([this]() {
+                std::function<void()> f;
+                for (;;) {
+                    {
+                        std::unique_lock<std::mutex> u_lock(_mutex);
+                        _cond_var.wait(u_lock, [this](){ return !_run || !_deque.empty(); });
+                        if (!_run && _deque.empty()) {
+                            return ;
+                        }
+                        f = std::move(_deque.back());
+                        _deque.pop_back();
+                    }
+                    f();
+                }
+            });
+        }
+    }
+
+    ~ThreadPool() {
+        {
+            std::unique_lock<std::mutex> u_lock(_mutex);
+            _run = false;
+        }
+        _cond_var.notify_all();
+        std::for_each(_workers.begin(), _workers.end(), [](auto & x) {
+            x.join();
+        });
+    }
+
+    template<typename T, typename... Args>
+    auto submit(T && f, Args &&... args) -> std::future<decltype(f(args...))> {
+        auto task = std::make_shared<std::packaged_task<decltype(f(args...))()>>(
+                std::bind(std::forward<T>(f), std::forward<Args>(args)...)
+        );
+        {
+            std::unique_lock<std::mutex> u_lock(_mutex);
+            _deque.emplace_front([task]() {
+                (*task)();
+            });
+        }
+        _cond_var.notify_one();
+        return task->get_future();
+    }
+
+private:
+    std::vector<std::thread>            _workers;
+    bool                                _run;
+    std::deque<std::function<void()>>   _deque;
+    std::mutex                          _mutex;
+    std::condition_variable             _cond_var;
+};
+
+#endif //THREAD_POOL_THREADPOOL_HH
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,18 @@
 // Created by kevin on 28/04/17.
 //
 
+#include <iostream>
+#include <future>
+#include "ThreadPool.hh"
+
 int main()
 {
+    ThreadPool p(5);
+
+    for (int i = 0; i < 100; ++i) {
+        p.submit([i](){
+            std::cout << i << std::endl;
+        });
+    }
     return 0;
 }
